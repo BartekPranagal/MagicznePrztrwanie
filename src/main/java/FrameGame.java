38bbfdf4b1da25048be3d8efd93f3dabc4a8e792
BartekPranagal <bpranagal@gmail.com>
2021-09-25 14:15:06 +0200

    dodanie tła do gry

--- src/main/java/FrameGame.java
+++ src/main/java/FrameGame.java
@@ -1,6 +1,21 @@
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class FrameGame extends JFrame {
 
+    PanelGame pg;
+    public FrameGame(PanelGame pg) {
+        this.pg =pg;
+        setVisible(true);
+        setTitle("Game");
+        setSize(pg.getPreferredSize());
+        setDefaultCloseOperation(3);
+        setLocationRelativeTo(null);
+        add(pg);
+        pack();
+    }
 }
 
