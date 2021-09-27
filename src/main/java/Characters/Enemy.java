9d10be3dfd3c7ab25fd147eb5e3eec1c81aa7570
agorn <agorny112@gmail.com>
2021-09-27 16:47:25 +0200

    ogólna klasa dla wrogów, trzeba się zastanowić czy rospisać ich pojedyńczo czy załatwić to enumem

---
+++ src/main/java/Characters/Enemy.java
@@ -0,0 +1,6 @@
+package Characters;
+
+public class Enemy extends Character{ // klasa wróg, nie wiem czy lepsze rozwiązanie to wypisanie wrogów,
+                                      // czy użycie Enum
+    private EnemyType enemyType;
+}
