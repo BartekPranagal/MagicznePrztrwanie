aea5c4c0160e78f150b2bf8bdc0ff099609f9cb9
agorn <agorny112@gmail.com>
2021-09-27 16:50:38 +0200

    tutaj będzie odbywało sięcałe sprawdzanie i animacja

---
+++ src/main/java/Game.java
@@ -0,0 +1,19 @@
+import Characters.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    Enemy enemy = new Enemy();
+    Hero bohater = new Hero();
+
+    List<Enemy> enemies = new ArrayList<Enemy>();
+
+
+
+
+
+
+}
