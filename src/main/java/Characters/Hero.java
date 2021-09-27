471529ef234c3c374ab8d752021da9b3a3d2ac09
agorn <agorny112@gmail.com>
2021-09-27 16:50:10 +0200

    nasz bohater

---
+++ src/main/java/Characters/Hero.java
@@ -0,0 +1,52 @@
+package Characters;
+
+import Skills.*;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hero extends Character{ // klasa bohater
+
+    //private int level;
+
+    private int currentExp;
+    private Level level;
+
+    private BufferedImage heroImage;
+
+    private Map<String, Skill> magicBook = new HashMap<String, Skill>();
+
+
+    public int getCurrentExp() {
+        return currentExp;
+    }
+
+    public void setCurrentExp(int currentExp) {
+        this.currentExp = currentExp;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public BufferedImage getHeroImage() {
+        return heroImage;
+    }
+
+    public void setHeroImage(BufferedImage heroImage) {
+        this.heroImage = heroImage;
+    }
+
+    public Map<String, Skill> getMagicBook() {
+        return magicBook;
+    }
+
+    public void setMagicBook(Map<String, Skill> magicBook) {
+        this.magicBook = magicBook;
+    }
+}
