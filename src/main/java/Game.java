288b7a4a9529b864dc666bafdd62aa25b622ad4d
agorn <agorny112@gmail.com>
2021-09-30 05:20:44 +0200

    metoda animacji , gdzie wrogowie podazaja za bohaterem

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
 import Characters.*;
 
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,17 +9,35 @@ public class Game {
 
     Enemy enemy = new Enemy();
     Hero bohater;
-
     List<Enemy> enemies = new ArrayList<Enemy>();
+    Point cursor;
+
 
 
     public Game() {
 
         bohater = new Hero();
-        bohater.setX(50);
-        bohater.setY(50);
-        bohater.setCharWidth(50);
-        bohater.setCharHeight(50);
-        bohater.setSpeed(5);
+        bohater.setX(400);
+        bohater.setY(400);
+        bohater.setWidth(50);
+        bohater.setHeight(50);
+        bohater.setSpeed(2);
+        bohater.setMovementSpeed(10);
+        sampleEnemies();
     }
+
+    public void sampleEnemies() {
+
+        enemies.add(new Enemy(EnemyType.FAT));
+        enemies.add(new Enemy(EnemyType.NORMAL));
+        enemies.add(new Enemy(EnemyType.DIVIDER));
+        enemies.add(new Enemy(EnemyType.SPEEDY));
+        enemies.add(new Enemy(EnemyType.KABOOM));
+        enemies.add(new Enemy(EnemyType.SPEEDY));
+        enemies.add(new Enemy(EnemyType.SPEEDY));
+        enemies.add(new Enemy(EnemyType.SPEEDY));
+
+
+    }
+
 }
