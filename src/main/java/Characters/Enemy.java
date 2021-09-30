68eb0f863eecd4355c982a588df0da7a9cf226d2
agorn <agorny112@gmail.com>
2021-09-30 05:18:07 +0200

    rozbudowa i podział przeciwników, narazie to zwykłe koła

--- src/main/java/Characters/Enemy.java
+++ src/main/java/Characters/Enemy.java
@@ -1,6 +1,56 @@
 package Characters;
 
-public class Enemy extends Character{ // klasa wróg, nie wiem czy lepsze rozwiązanie to wypisanie wrogów,
-                                      // czy użycie Enum
+import java.awt.*;
+
+public class Enemy extends Character { // klasa wróg, nie wiem czy lepsze rozwiązanie to wypisanie wrogów,
+    // czy użycie Enum
     private EnemyType enemyType;
+
+    public Enemy(EnemyType enemyType) {
+        setX((int) (Math.random() * 1000));
+        setY(0);
+        setWidth(enemyType.getWidth());
+        setHeight(enemyType.getHeight());
+        setArmor(enemyType.getArmor());
+        setBaseDmg(enemyType.getBaseDmg());
+        setMaxHp(enemyType.getMaxHp());
+        setSpeed(enemyType.getSpeed());
+        this.enemyType = enemyType;
+
+    }
+
+    public Enemy() {
+
+    }
+
+    public EnemyType getEnemyType() {
+        return enemyType;
+    }
+
+    public void drawEnemy(Graphics g, EnemyType eT, int x, int y) {
+        if (eT == EnemyType.FAT) {
+            g.setColor(Color.BLACK);
+            g.fillOval(x, y, EnemyType.FAT.getWidth(), EnemyType.FAT.getHeight());
+        }
+        if (eT == EnemyType.NORMAL) {
+            g.setColor(Color.BLUE);
+            g.fillOval(x, y, EnemyType.NORMAL.getWidth(), EnemyType.NORMAL.getHeight());
+
+        }
+        if (eT == EnemyType.DIVIDER) {
+            g.setColor(Color.CYAN);
+            g.fillOval(x, y, EnemyType.DIVIDER.getWidth(), EnemyType.DIVIDER.getHeight());
+        }
+        if (eT == EnemyType.KABOOM) {
+            g.setColor(Color.RED);
+            g.fillOval(x, y, EnemyType.KABOOM.getWidth(), EnemyType.KABOOM.getHeight());
+        }
+        if (eT == EnemyType.SPEEDY) {
+            g.setColor(Color.pink);
+            g.fillOval(x, y, EnemyType.SPEEDY.getWidth(), EnemyType.SPEEDY.getHeight());
+
+        }
+
+
+    }
 }
