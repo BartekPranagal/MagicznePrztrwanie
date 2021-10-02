6053f7a6474f0588a493289411e81d4237084fd6
agorny112 <agorny112@gmail.com>
2021-10-02 05:14:23 +0200

    dodanie getterów i setterów, oraz hitboxa

--- src/main/java/Skills/Skill.java
+++ src/main/java/Skills/Skill.java
@@ -1,5 +1,6 @@
 package Skills;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Skill {
@@ -7,13 +8,78 @@ public class Skill {
     private String name;
 
     private int x,y;
-    private int skillwidth,skillHeight;
+    private int width, height;
 
     private int dmg;
     private int skillLevel;
 
     private BufferedImage skillImage;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getDmg() {
+        return dmg;
+    }
+
+    public void setDmg(int dmg) {
+        this.dmg = dmg;
+    }
+
+    public int getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(int skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public BufferedImage getSkillImage() {
+        return skillImage;
+    }
+
+    public void setSkillImage(BufferedImage skillImage) {
+        this.skillImage = skillImage;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x,y,width,height);
+    }
 }
